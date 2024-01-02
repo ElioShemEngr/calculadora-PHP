05ad8f8beaa6280c4d5b4de8c4bbcda8218383e9
Elio Shem B <122136358+ElioShemEngr@users.noreply.github.com>
2024-01-01 20:18:25 -0500

    index.php

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
-<!DOCTYPE html>
-  <head>
-    <title>CALCULADORA</title>
-  </head>
-  <body>
-    <?php echo '<h1>Calculadora en PHP</h1>'; ?>
-    <?php echo '<h3>Seleccione la operación a realizar:</h3>'; ?>
-    <form action="index.php" method="post">
-      <label for="operacion">Operación:</label>
-      <select id="operacion" name="operacion">
-        <option value="suma">Suma</option>
-        <option value="resta">Resta</option>
-        <option value="multiplicacion">Multiplicación</option>
-        <option value="division">División</option>
-      </select>
-      <br><br>
-    </form>
+<?php
 
+function calculadora($valor1,$valor2,$op){
+  switch ($op){
+    case "+":
+      $resultado = $valor1 + $valor2;
+      echo "El resultado de la suma es: ".$resultado;
+      break;
+    case "-":
+      $resultado = $valor1 - $valor2;
+      echo "El resultado de la resta es: ".$resultado;
+      break;
+    case "*":
+      $resultado = $valor1 * $valor2;
+      echo "El resultado de la multiplicación es: ".$resultado;
+      break;
+    case "/":
+      $resultado = $valor1 / $valor2;
+      echo "El resultado de la division es: ".$resultado;
+      break;
+    default:
+    echo "Operación no válida";
+    break;
+  }
+}
 
-    <form action="procesar_datos.php" method="post">
-      <label for="primer_numero">1ER NUMERO:</label>
-      <input type="text" id="nombre" name="nombre" required>
-      <br><br>
-      <label for="segundo_numero">2DO NUMERO::</label>
-      <input type="text" id="nombre" name="nombre" required>
-      <br><br>
-      <input type="submit" value="Enviar">
-    </form>
+$valor1 = readline( "Digite el primer valor: ");
+$valor2 = readline( "Digite el segundo valor: ");
+$op = readline("Seleccione +, -, *, /: ");
 
-  </body>
-</html>
+calculadora($valor1, $valor2, $op);
+?>
